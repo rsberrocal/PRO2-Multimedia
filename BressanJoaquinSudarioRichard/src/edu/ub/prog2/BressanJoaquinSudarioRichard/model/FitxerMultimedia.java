e9ccf71802186b221cd71dbaae83246033e72e2f
Richard <rsberrocal@gmail.com>
2019-02-19 13:53:39 +0100

    Created getters and setter clas File

---
+++ BressanJoaquinSudarioRichard/src/edu/ub/prog2/BressanJoaquinSudarioRichard/model/FitxerMultimedia.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.ub.prog2.BressanJoaquinSudarioRichard.model;
+
+import java.io.File;
+import java.util.Date;
+
+/**
+ *
+ * @author rsudarbe117.alumnes
+ */
+public class FitxerMultimedia extends File{  
+    
+    private String path;
+    private String nameFile;
+    private String ext;
+    private Date lastUpdate;
+    private String description;
+    
+    public FitxerMultimedia(String path) {
+        super(path);
+    }
+    
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getNameFile() {
+        return nameFile;
+    }
+
+    public void setNameFile(String nameFile) {
+        this.nameFile = nameFile;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public void setExt(String ext) {
+        this.ext = ext;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
