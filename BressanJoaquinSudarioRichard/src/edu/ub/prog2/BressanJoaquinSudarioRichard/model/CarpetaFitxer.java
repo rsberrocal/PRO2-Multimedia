bfede3924621196fd186ee2f19e9744536c7cbd1
mat.aules <mat.aules@Id47787.ub.edu>
2019-02-26 13:56:08 +0100

    Metodos acabados toString() Clear() isFull() get with position

--- BressanJoaquinSudarioRichard/src/edu/ub/prog2/BressanJoaquinSudarioRichard/model/CarpetaFitxer.java
+++ BressanJoaquinSudarioRichard/src/edu/ub/prog2/BressanJoaquinSudarioRichard/model/CarpetaFitxer.java
@@ -5,10 +5,63 @@
  */
 package edu.ub.prog2.BressanJoaquinSudarioRichard.model;
 
+import java.io.File;
+import java.util.ArrayList;
+
 /**
  *
  * @author rsudarbe117.alumnes
  */
 public class CarpetaFitxer {
+    //private int size;
+    private int MAX_SIZE = 100;
+    private ArrayList<FitxerMultimedia> folder;
     
+    public CarpetaFitxer() {
+        this.folder = new ArrayList<>();
+    }
+    
+    public int getSize(){       
+        return this.folder.size();
+    }
+    
+    public void addFitxer(File fitxer){
+        
+        //this.folder.add(fitxer);
+    }
+    
+    public void removeFitxer(File fitxer) throws Exception{
+        //CHECK FITXER IS FITXERMULTIMEDIA
+        int fileIndex = this.folder.indexOf(fitxer);
+        if(fileIndex!=-1)
+            this.folder.remove(fileIndex);
+        else
+            throw new Exception("ERROR:File not found");
+    }
+    
+    public File getAt(int position){
+        return this.folder.get(position);
+    }
+    
+    public void clear(){
+        this.folder.clear();
+    }
+    
+    public boolean isFull(){
+        return this.folder.size() == this.MAX_SIZE;
+    }
+    
+    @Override
+    public String toString(){
+        String message = "Carpeta Fitxers:\n";
+        message += "==============\n";
+        for(int i=0;i < this.folder.size();i++){
+            /*[1] Descripció= Nadal, data=Thu Jan 08 12:34:02 CET 2015, nom
+                fitxer=carmen, ext=mp4, cami complet=F:\carmen.mp4 */
+            FitxerMultimedia file = this.folder.get(i);
+            message += "["+(i+1)+"]  Descripció = "+file.getDescription()+", data = "+file.getLastUpdate()+", nom fitxer = "+file.getNameFile()+", ext = "+file.getExt()+", cami complet = "+file.getAbsolutePath();
+            message += "\n";
+        }       
+        return message;
+    }
 }
