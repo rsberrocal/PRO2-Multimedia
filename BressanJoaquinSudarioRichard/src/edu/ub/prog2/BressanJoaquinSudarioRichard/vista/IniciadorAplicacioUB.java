b09d3ffd0c80a8f63c81e2c96424a83448e23f5a
mat.aules <mat.aules@Id47787.ub.edu>
2019-02-26 13:08:32 +0100

    Finished Main Menu easy

--- BressanJoaquinSudarioRichard/src/edu/ub/prog2/BressanJoaquinSudarioRichard/vista/IniciadorAplicacioUB.java
+++ BressanJoaquinSudarioRichard/src/edu/ub/prog2/BressanJoaquinSudarioRichard/vista/IniciadorAplicacioUB.java
@@ -5,20 +5,57 @@
  */
 package edu.ub.prog2.BressanJoaquinSudarioRichard.vista;
 
+import java.util.Scanner;
+import edu.ub.prog2.utils.Menu;
+
 /**
  *
  * @author rsudarbe117.alumnes
  */
 public class IniciadorAplicacioUB {
+
     /**
      * @param args the command line arguments
      */
+    static private enum OptionsMainMenu {
+        MAIN_MENU_OPTION1, MAIN_MENU_OPTION2, MAIN_MENU_EXIT
+    };
+
+    static private String[] descMainMenu = {"Option 1", "Option 2", "Option 3", "Exit"};
+
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner sc = new Scanner(System.in);
+
+        IniciadorAplicacioUB aplicacio = new IniciadorAplicacioUB();
+
+        aplicacio.gestioAplicacioUB(sc);
     }
- 
-    public void gestioAplicacioUB(){
-        
+
+    public void gestioAplicacioUB(Scanner sc) {
+
+        Menu<OptionsMainMenu> mainMenu = new Menu<OptionsMainMenu>("Main Menu", OptionsMainMenu.values());
+
+        mainMenu.setDescripcions(descMainMenu);
+
+        OptionsMainMenu option = null;
+        do {
+            mainMenu.mostrarMenu();
+            option = mainMenu.getOpcio(sc);
+            
+            switch(option){
+                case MAIN_MENU_OPTION1:
+                    System.out.println("Option 1");
+                    break;
+                case MAIN_MENU_OPTION2:
+                    System.out.println("Option 2");
+                    break;
+                    case MAIN_MENU_EXIT:
+                        System.out.println("Good Bye");
+                        break;
+            }
+            
+        } while (option != OptionsMainMenu.MAIN_MENU_EXIT);
+
     }
 }
