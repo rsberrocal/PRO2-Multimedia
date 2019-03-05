bc19d4b05f749566eafe76b8dfd9fc6fc60744e9
joaqu <joaqu@LAPTOP-7AU7AS5L>
2019-03-05 13:57:20 +0100

    Main Menu

--- BressanJoaquinSudarioRichard_L2/src/ub/edu/prog2/BressanJoaquinSudarioRichard/vista/IniciadorAplicacioUB.java
+++ BressanJoaquinSudarioRichard_L2/src/ub/edu/prog2/BressanJoaquinSudarioRichard/vista/IniciadorAplicacioUB.java
@@ -13,7 +13,20 @@ import java.util.Scanner;
  * @author joaqu
  */
 public class IniciadorAplicacioUB {
-
+    static private enum OpcionsMainMenu{
+        MAIN_MENU_OPTION1, MAIN_MENU_OPTION2, MAIN_MENU_OPTION3, MAIN_MENU_EXIT
+    };
+    static private enum OpcionsSubMenu{
+        SUB_MENU_OPTION1, SUB_MENU_OPTION2, SUB_MENU_OPTION3, SUB_MENU_EXIT
+    };
+    static private enum OpcionsSubMenu1{
+        SUB_MENU1_OPTION1, SUB_MENU1_OPTION2, SUB_MENU1_OPTION3, SUB_MENU1_EXIT
+    }
+    
+    static private String[] descMainMenu = {"Gestio biblioteca", "Guardar dades", "Recuperar dades", "Sortir"};
+    static private String[] descSubMenu = {"Afegir fitxer multimedia", "Mostrar carpeta", "Eliminar fitxer multimedia", "Menu anterior"};
+    static private String[] descSubMenu1 = {"Afegir video", "Afegir audio", "Menu anterior"};
+    
     /**
      * @param args the command line arguments
      */
@@ -26,6 +39,6 @@ public class IniciadorAplicacioUB {
     }
     
     public void gestioAplicacioUB(Scanner sc){
-        Menu<OpcionsMainMenu,OpcionsSubMenu,OpcionsSubMenu1> mainMenu = new Menu("Menu", {OpcionsMainMenu,OpcionsSubMenu,OpcionsSubMenu1)
+        Menu<OpcionsMainMenu> mainMenu = new Menu("Menu", {OpcionsMainMenu,OpcionsSubMenu,OpcionsSubMenu1});
     }
 }
