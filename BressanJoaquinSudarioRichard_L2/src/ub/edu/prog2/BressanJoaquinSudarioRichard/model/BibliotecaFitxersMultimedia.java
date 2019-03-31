3512547d4299d26c65ec811c2cf686e7643a5837
rsberrocal <rsberrocal@gmail.com>
2019-03-31 19:34:20 +0200

    Fixed Add and created remove

--- BressanJoaquinSudarioRichard_L2/src/ub/edu/prog2/BressanJoaquinSudarioRichard/model/BibliotecaFitxersMultimedia.java
+++ BressanJoaquinSudarioRichard_L2/src/ub/edu/prog2/BressanJoaquinSudarioRichard/model/BibliotecaFitxersMultimedia.java
@@ -8,6 +8,8 @@ package ub.edu.prog2.BressanJoaquinSudarioRichard.model;
 import java.io.File;
 import java.util.Scanner;
 import edu.ub.prog2.utils.AplicacioException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -20,50 +22,24 @@ public class BibliotecaFitxersMultimedia extends CarpetaFitxers {
     }
 
     public void addFitxer(File fitxer, boolean isAudio) throws AplicacioException {
-        if (this.isFull()) {
-            throw new AplicacioException("Error: biblioteca plena");
+        try {
+            super.addFitxer(fitxer);
+        } catch (Exception e) {
+            throw new AplicacioException(e.getMessage());
+        }
+    }
+
+    public void removeFitxer(int index) throws AplicacioException {
+        if (this.getSize() == 0) {
+            throw new AplicacioException("Error; Biblioteca buida");
         } else {
-            String desc;
-            float durat;
-            String codec;
-            char optionFile;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Afegeix una descripcion al fitxer " + fitxer.getName());
-            desc = sc.next();
-            System.out.println("Afegeix una durada al fitxer " + fitxer.getName());
-            durat = sc.nextFloat();
-            System.out.println("Afegeix un codec al fitxer " + fitxer.getName());
-            codec = sc.next();
-            System.out.println("Sera un audio o video? A/V");
-            optionFile = sc.next().charAt(0);
-            if (isAudio) {
-                int kbs;
-                String imgFile;
-                System.out.println("Afeigeix una qualitat d'audio(kbs)");
-                kbs = sc.nextInt();
-                System.out.println("Afegeix la ruta de la imatge al audio");
-                imgFile = sc.next();
-                File f = new File(imgFile);
-                try {
-                    super.addFitxer(new Audio(fitxer.getAbsolutePath(), f, desc, codec, durat, kbs, new Reproductor()));
-                } catch (Exception e) {
-                    throw new AplicacioException(e.getMessage());
-                }
+            if (index > this.getSize()) {
+                throw new AplicacioException("Error: posició incorrecta");
             } else {
-                int width;
-                int height;
-                float fps;
-                System.out.println("Dimensions del video");
-                System.out.println("Amplada:");
-                width = sc.nextInt();
-                System.out.println("Alçada:");
-                height = sc.nextInt();
-                System.out.println("Afeigeix els frames per segon del video");
-                fps = sc.nextFloat();
                 try {
-                    super.addFitxer(new Video(fitxer.getAbsolutePath(), desc, codec, durat, height, width, fps, new Reproductor()));
-                } catch (Exception e) {
-                    throw new AplicacioException(e.getMessage());
+                    super.removeFitxer(super.getAt(index));
+                } catch (Exception ex) {
+                    throw new AplicacioException(ex.getMessage());
                 }
             }
         }
