080afb78cb2b03b92c6a5f4c257e0013493f2adf
jbressannazar <joaquinbressan@gmail.com>
2019-03-31 20:24:47 +0200

    Controlador

--- BressanJoaquinSudarioRichard_L2/src/ub/edu/prog2/BressanJoaquinSudarioRichard/controlador/Controlador.java
+++ BressanJoaquinSudarioRichard_L2/src/ub/edu/prog2/BressanJoaquinSudarioRichard/controlador/Controlador.java
@@ -7,9 +7,10 @@ package ub.edu.prog2.BressanJoaquinSudarioRichard.controlador;
 
 import ub.edu.prog2.BressanJoaquinSudarioRichard.model.Dades;
 import edu.ub.prog2.utils.AplicacioException;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
+import ub.edu.prog2.BressanJoaquinSudarioRichard.model.Reproductor;
 /**
  *
  * @author joaqu
@@ -17,24 +18,23 @@ import java.util.List;
 public class Controlador {
     
     Dades d;
+    Reproductor r;
     
     public void afegirVideo (String path, String nomVideo, String codec, float durada, int alcada, int amplada, float fps) throws AplicacioException{
-        
+        d.addVideo(path, nomVideo, codec, durada, alcada, amplada, fps);
     }
     
-    public void afegirAudio(String cami, String camilmatge, String nomAudio, String codec, float durada, int kbps) throws AplicacioException{
-        
+    public void afegirAudio(String cami, File camiImatge, String nomAudio, String codec, float durada, int kbps) throws AplicacioException, Exception{
+        d.addAudio(cami, camiImatge, nomAudio, codec, durada, kbps);
     }
     
     public List<String> mostrarBiblioteca(){ // lista de las salidas de toString() de los ficheros
-        List<String> d = null; //Sino aparecía como error
-        return d;
+        return d.print();
+            
     }
     
     public void esborrarFitxer(int id) throws AplicacioException{
-        // id es la posición en la lista de getBiblioteca()
-        
-        
+        d.delete(id);
     }
     
     public void guardarDadesDisc(String camiDesti) throws AplicacioException, IOException{
